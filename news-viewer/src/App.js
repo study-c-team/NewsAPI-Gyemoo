abfc6867e433df4b49a9afe0073b5bd8ccd8676e
Gyemoo <lgm528@naver.com>
2023-05-28 16:38:22 +0900

    Categories

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import NewList from './components/NewList';
+import React, { useCallback, useState } from 'react';
+import Categories from './components/Categories';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  return <NewList />;
-}
+
+  // 기본 카테고리 state 선언
+  const [category, setCategory] = useState('all')
+  // 콜백으로 사용 할 카테고리 함수
+  const onSelect = useCallback(Category => setCategory(Category), [])
+
+  return (
+    <>
+      {/* props로 카테고리 state와 함수를 넘겨줌 */}
+      <Categories  category={category} onSelect={onSelect} />
+      <NewsList category={category}/>
+    </>
+  );
+};
 
 export default App
--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,36 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem"
+import axios from "axios";
 
 const NewsItemBlock = styled.div`
     box-sizing: border-box;
     padding-bottom: 3rem;
-    width: 768px:
+    width: 768px;
     margin: 0 auto;
     margin-top: 2rem;
     @media screen and (max-width: 768px) {
-        wdith: 100%;
+        width: 100%;
         padding-left: 1rem;
         padding-right: 1rem;
     }
 `;
 
-const sampleArticle = {
-    title: '제목',
-    description: '내용', 
-    url: 'https://google.com', 
-    urlToImage: 'https://via.placeholder.com/160',
-};
+const NewsList = ({category} ) => {
+    const [ articles, setArticles ] = useState(null);
+    const [ loading , setLoading ] = useState(null);
 
-const NewsList = () => {
+    useEffect(() => {
+        // async 비동기 함수호출
+        const fetchData = async () => {
+            // APi 호출 시간동안 보여줄 로딩바
+            setLoading(true)
+            // try catch문 에러 처리
+            try {
+                // props로 넘어온 state로 
+                const query = category === 'all' ? '' : `&category=${category}`;
+                const response = await axios.get(
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=afda249a1ccb482fa0944d12a295021b`,
+                );
+                // API 데이터 state 저장
+                setArticles(response.data.articles)
+            } catch (e) {
+                console.log(e)
+            }
+            setLoading(false)
+        };
+        fetchData();
+    }, [category]);
+
+    // 대기 중
+    if (loading) {
+        return <NewsItemBlock>대기 중입니다...</NewsItemBlock>
+    }
+    // articles 값이 설정 안될경우 (null 오류방지)
+    if (!articles) {
+        return null;
+    }
+
+    // articles 같이 유효할 떄
     return (
         <NewsItemBlock>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
+            {articles.map(v =>  (
+                <NewsItem key={v.url} article={v} />
+            ))}
         </NewsItemBlock>
     );
 };
